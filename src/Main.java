f1ecde0149240f9e7099435bf141986aacbbc70e
Pragnya Paramita Pattanaik <pragnya.pattanaik@gmail.com>
2023-06-02 23:48:41 -0700

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+
+
+        Vehicle vehicle = new Vehicle();
+        Car car = new Car();
+        vehicle.drive();
+        car.drive();
+    }
+
+
+
+}
